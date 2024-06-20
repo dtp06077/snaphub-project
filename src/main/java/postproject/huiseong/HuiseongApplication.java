26e92adf7092cdee8b8041ef194143b965a51eab
dtp06077 <dtp06077@gmail.com>
2024-06-21 01:17:11 +0900

    INIT COMMIT

---
+++ src/main/java/postproject/huiseong/HuiseongApplication.java
@@ -0,0 +1,13 @@
+package postproject.huiseong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiseongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuiseongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/postproject/huiseong/HuiseongApplicationTests.java
@@ -0,0 +1,13 @@
+package postproject.huiseong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiseongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
