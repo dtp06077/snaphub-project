5f5711c3ab63e496f50f4a04443cb0cddae6a7f6
dtp06077 <dtp06077@gmail.com>
2024-06-29 07:39:12 +0900

    FEAT : Emotion 도메인 클래스 추가

--- src/main/java/project/backend/domain/Emotion.java
+++ src/main/java/project/backend/domain/Emotion.java
@@ -1,4 +1,21 @@
 package project.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Emotion {
+
+    @Id @GeneratedValue
+    @Column(name = "emotion_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Enumerated(EnumType.STRING)
+    private EmotionStatus status;
 }
