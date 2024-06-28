41564b436af58038fc69baa1a146b15a0e6ea84e
dtp06077 <dtp06077@gmail.com>
2024-06-29 08:16:10 +0900

    FIX : UserRepository.findByName 오류 수정

--- src/main/java/project/backend/repository/UserRepository.java
+++ src/main/java/project/backend/repository/UserRepository.java
@@ -26,6 +26,7 @@ public class UserRepository {
     public User findByName(String name) {
         return em.createQuery("select u from User u where u.name = :name",
                 User.class)
+                .setParameter("name", name)
                 .getSingleResult();
     }
 
