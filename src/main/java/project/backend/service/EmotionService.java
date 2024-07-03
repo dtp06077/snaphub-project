13b798abf006517e47e4b181e46e6d0797d1a1e3
dtp06077 <dtp06077@gmail.com>
2024-07-04 03:05:37 +0900

    FEAT : EmotionService 생성 및 메서드 추가

--- src/main/java/project/backend/service/EmotionService.java
+++ src/main/java/project/backend/service/EmotionService.java
@@ -3,7 +3,10 @@ package project.backend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.backend.domain.Emotion;
 import project.backend.repository.EmotionRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +15,26 @@ public class EmotionService {
 
     private final EmotionRepository emotionRepository;
 
+    /**
+     * 감정표현 저장
+     */
+    @Transactional
+    public Long saveEmotion(Emotion emotion) {
+        return emotionRepository.save(emotion);
+    }
 
+    /**
+     * 감정표현 삭제
+     */
+    @Transactional
+    public void deleteEmotion(Long id) {
+        emotionRepository.delete(id);
+    }
+
+    /**
+     * 특정 게시글의 감정표현 검색
+     */
+    public List<Emotion> getEmosByPostId(Long postId) {
+        return emotionRepository.findByPostId(postId);
+    }
 }
