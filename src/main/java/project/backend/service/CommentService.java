b8cbdd8a669779ebbbe686556467c8282b577d67
dtp06077 <dtp06077@gmail.com>
2024-07-04 02:53:44 +0900

    FEAT : CommentService 메서드 설명 추가

--- src/main/java/project/backend/service/CommentService.java
+++ src/main/java/project/backend/service/CommentService.java
@@ -15,19 +15,31 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
+    /**
+     * 댓글 저장
+     */
     @Transactional
     public Long saveComment(Comment comment) {
         return commentRepository.save(comment);
     }
 
+    /**
+     * 댓글 검색
+     */
     public Comment getComment(Long id) {
         return commentRepository.findById(id);
     }
 
-    public List<Comment> getCommentByPostId(Long postId) {
+    /**
+     *특정 게시글의 댓글 검색
+     */
+    public List<Comment> getCommentsByPostId(Long postId) {
         return commentRepository.findByPostId(postId);
     }
 
+    /**
+     * 댓글 삭제
+     */
     @Transactional
     public void deleteComment(Long id) {
         commentRepository.delete(id);
