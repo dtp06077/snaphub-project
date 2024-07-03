0026d9f8281e83526989ed558d1aeaea93a8237b
dtp06077 <dtp06077@gmail.com>
2024-07-04 02:43:36 +0900

    FEAT : CommentService 생성 및 메서드 추가

--- src/main/java/project/backend/service/CommentService.java
+++ src/main/java/project/backend/service/CommentService.java
@@ -3,7 +3,10 @@ package project.backend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.backend.domain.Comment;
 import project.backend.repository.CommentRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +15,21 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
+    @Transactional
+    public Long saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
 
+    public Comment getComment(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    public List<Comment> getCommentByPostId(Long postId) {
+        return commentRepository.findByPostId(postId);
+    }
+
+    @Transactional
+    public void deleteComment(Long id) {
+        commentRepository.delete(id);
+    }
 }
