4b27554f4c77ee2b6f6c91ead4f42ea4397c3666
dtp06077 <dtp06077@gmail.com>
2024-07-09 17:16:05 +0900

    FEAT : SecurityConfig 생성

--- src/main/java/project/backend/controller/IndexController.java
+++ src/main/java/project/backend/controller/IndexController.java
@@ -2,6 +2,7 @@ package project.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -12,22 +13,23 @@ public class IndexController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public @ResponseBody String user() {
         return "user";
     }
 
     @GetMapping("/admin")
-    public String admin() {
+    public @ResponseBody String admin() {
         return "admin";
     }
 
+    //스프링 시큐리티가 login 주소를 가져감.
     @GetMapping("/login")
-    public String login() {
+    public @ResponseBody String login() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String register() {
+    public @ResponseBody String register() {
         return "register";
     }
 }
