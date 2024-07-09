d6d105ba866a556405c1aea2ab2519e0e0da7038
dtp06077 <dtp06077@gmail.com>
2024-07-09 16:34:15 +0900

    TEST : IndexController 테스트

--- src/main/java/project/backend/controller/IndexController.java
+++ src/main/java/project/backend/controller/IndexController.java
@@ -1,13 +1,33 @@
 package project.backend.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
