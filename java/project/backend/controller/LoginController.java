6912a50530b37774469ddcf837602010872ab5f2
dtp06077 <dtp06077@gmail.com>
2024-07-14 00:55:27 +0900

    TEST : JWT 토큰 생성 및 해석 테스트

--- java/project/backend/controller/LoginController.java
+++ java/project/backend/controller/LoginController.java
@@ -1,15 +1,14 @@
 package project.backend.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.backend.constants.SecurityConstants;
 import project.backend.domain.AuthenticationRequest;
 import project.backend.prop.JwtProp;
@@ -25,7 +24,7 @@ public class LoginController {
     @Autowired
     private JwtProp jwtProp;
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest request) {
         String username = request.getUsername();
         String password = request.getPassword();
@@ -44,18 +43,48 @@ public class LoginController {
         //토큰 생성
         String jwt = Jwts.builder()
                         // .signWith( 시크릿키 , 알고리즘 )
-                        .signWith(Keys.hmacShaKeyFor(signingKey), Jwts.SIG.HS512 )
-                        .header()
-                            .add("type", SecurityConstants.TOKEN_TYPE)
+                        .signWith(Keys.hmacShaKeyFor(signingKey), Jwts.SIG.HS512 )  // 시그니처 사용할 비밀키, 알고리즘 설정
+                        .header()                                                   // 헤더 설정
+                            .add("type", SecurityConstants.TOKEN_TYPE)           // type : JWT
                         .and()
-                        .expiration(new Date( System.currentTimeMillis() + 1000*60*60*24*5 ))
-                        .claim("uid", username)
-                        .claim("rol", roles)
-                        .compact();
+                        .expiration(new Date( System.currentTimeMillis() + 1000*60*60*24*5 )) // 토큰 만료 시간 설정 (5일)
+                        .claim("uid", username)                                  //PAYLOAD - uid : user   (사용자 아이디)
+                        .claim("role", roles)                                     //PAYLOAD - rol : [ROLE_USER, ROLE_ADMIN] (권한 정보)
+                        .compact();                                                 //최종적으로 토큰 생성
 
         log.info("jwt : " +jwt);
 
         return new ResponseEntity<String>(jwt, HttpStatus.OK);
+    }
+
+    //토큰 해석
+    @GetMapping("/user/info")
+    public ResponseEntity<?> userInfo(@RequestHeader(name="Authorization") String header) {
+
+        log.info("===== header ======");
+        log.info("Authorization : " + header);
+
+        // Authorization : Bearer ${jwt}
+        String jwt = header.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+        byte[] signingKey = jwtProp.getSecret().getBytes();
+
+        Jws<Claims> parsedToken = Jwts.parser()
+                                    .verifyWith(Keys.hmacShaKeyFor(signingKey))
+                                    .build()
+                                    .parseSignedClaims(jwt);
+
+        log.info("parsedToken : "+ parsedToken);
+
+        //uid = user
+        String username = parsedToken.getPayload().get("uid").toString();
+        log.info("username : " + username);
+
+        Claims claims = parsedToken.getPayload();
+        Object roles = claims.get("role");
+        log.info("roles : " + roles);
+
+        return new ResponseEntity<String>(parsedToken.toString(), HttpStatus.OK);
     }
 
 }
