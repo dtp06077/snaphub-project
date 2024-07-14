c1615a0f0f6eb94c8ac0f79724a96a6f872381a2
dtp06077 <dtp06077@gmail.com>
2024-07-15 02:17:57 +0900

    FEAT : Repository 상위 클래스 생성

--- java/project/server/repository/Repository.java
+++ java/project/server/repository/Repository.java
@@ -2,4 +2,13 @@ package project.server.repository;
 
 public interface Repository {
 
+    //엔티티 저장
+    public Long save(Object entity);
+
+    //엔티티 조회
+    public Object findById(Long id);
+
+
+    // 삭제
+    public void remove(Long id);
 }
