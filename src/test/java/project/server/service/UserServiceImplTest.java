6494d432782e835f6626aaade57697f402194623
dtp06077 <dtp06077@gmail.com>
2024-07-15 21:38:51 +0900

    TEST : 회원 가입 성공 테스트, 로그인 테스트

--- src/test/java/project/server/service/UserServiceImplTest.java
+++ src/test/java/project/server/service/UserServiceImplTest.java
@@ -1,18 +1,26 @@
 package project.server.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import project.server.domain.User;
+import project.server.domain.UserAuth;
+import project.server.dto.LoginRequest;
+import project.server.dto.UserRequest;
 import project.server.repository.UserRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -20,86 +28,97 @@ import static org.assertj.core.api.Assertions.fail;
 public class UserServiceImplTest {
 
     @Autowired
-    private UserServiceImpl userServiceImpl;
+    private UserService userServiceImpl;
 
     @Autowired
     private UserRepository userRepository;
 
-    private User user;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private HttpServletRequest mockRequest;
+
+    private UserRequest userRequest;
 
     @BeforeEach
     void setUp() {
-        user = new User();
-        user.setName("HuiSeong");
-        user.setLoginId("testLoginId");
-        user.setPassword("testPassword");
+        userRequest = new UserRequest();
+        userRequest.setName("HuiSeong");
+        userRequest.setLoginId("testLoginId");
+        userRequest.setPassword("testPassword");
+        userRequest.setEmail("");
+        userRequest.setProfile("");
     }
 
     @Test
     @DisplayName("회원 가입 성공 테스트")
-    public void register_success() {
+    public void insert_success() throws Exception {
         //Given
-        Long userId = userServiceImpl.register(user);
+        Long userId = userServiceImpl.insert(userRequest);
 
         //When
         User findUser = userRepository.findById(userId);
+        UserAuth auth = findUser.getAuths().get(0);
 
         // Then
-        assertThat(user).isEqualTo(findUser);
+        System.out.println(findUser.getPassword());
+        assertThat(findUser.getName()).isEqualTo("HuiSeong");
+        assertThat(auth.getAuth()).isEqualTo("ROLE_USER");
     }
 
     @Test
     @DisplayName("로그인 성공 테스트")
-    public void login_success() {
+    public void login_success() throws Exception {
         //Given
-        Long userId = userServiceImpl.register(user);
+        Long userId = userServiceImpl.insert(userRequest);
+
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId(userRequest.getLoginId());
+        loginRequest.setPassword(userRequest.getPassword());
 
         //When
-        User findUser = userServiceImpl.login("testLoginId", "testPassword");
+        userServiceImpl.login(loginRequest, mockRequest);
 
-        //Then
-        assertThat(userRepository.findById(userId)).isEqualTo(findUser);
+        // Then
+        User findUser = userRepository.findById(userId);
+        assertThat(findUser).isNotNull();
     }
 
     @Test
-    @DisplayName("로그인 실패 테스트 - 아이디")
-    public void login_fail_wrongLoginId() {
+    @DisplayName("로그인 실패 테스트 - 잘못된 비밀번호")
+    public void login_fail_wrong_password() throws Exception {
         //Given
-        Long userId = userServiceImpl.register(user);
+        userServiceImpl.insert(userRequest);
 
-        //When
-        try {
-            User findUser = userServiceImpl.login("wrongId", "testPassword");
-        } catch (IllegalStateException e) {
-            return;
-        }
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId(userRequest.getLoginId());
+        loginRequest.setPassword("wrongPassword");
 
-        //Then
-        fail("로그인 실패 예외가 발생해야 한다.");
+        // When & Then
+        assertThrows(AuthenticationException.class, () -> {
+            userServiceImpl.login(loginRequest, mockRequest);
+        });
     }
 
     @Test
-    @DisplayName("로그인 실패 테스트 - 비밀번호")
-    public void login_fail_wrongPassword() {
-        //Given
-        Long userId = userServiceImpl.register(user);
+    @DisplayName("로그인 실패 테스트 - 존재하지 않는 사용자")
+    public void login_fail_nonexistent_user() throws Exception {
+        // Given
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setLoginId("nonexistentUser");
+        loginRequest.setPassword("testPassword");
 
-        //When
-        try {
-            User findUser = userServiceImpl.login("testLoginId", "wrongPassword");
-        } catch (IllegalStateException e) {
-            return;
-        }
-
-        //Then
-        fail("로그인 실패 예외가 발생해야 한다.");
-    }
+        // When & Then
+        assertThrows(AuthenticationException.class, () -> {
+            userServiceImpl.login(loginRequest, mockRequest);
+        });
 
     @Test
     @DisplayName("프로필 업데이트 성공 테스트")
-    public void updateProfile_success() {
+    public void updateProfile_success() throws Exception {
         //Given
-        Long userId = userServiceImpl.register(user);
+        Long userId = userServiceImpl.insert(user);
 
         //When
         userServiceImpl.updateProfile(userId, "abcd@email.com", "A#$!");
@@ -111,7 +130,7 @@ public class UserServiceImplTest {
 
     @Test
     @DisplayName("프로필 업데이트 실패 테스트")
-    public void updateProfile_fail() {
+    public void updateProfile_fail() throws Exception {
         //Given
 
         //When
