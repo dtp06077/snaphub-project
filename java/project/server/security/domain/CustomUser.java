d3027e6798502ecf7ba63cb91b44cf7b4ea43864
dtp06077 <dtp06077@gmail.com>
2024-07-17 17:15:17 +0900

    REFACTOR : CustomUser Getter Setter 추가

--- java/project/server/security/domain/CustomUser.java
+++ java/project/server/security/domain/CustomUser.java
@@ -1,6 +1,8 @@
 package project.server.security.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Getter @Setter
 public class CustomUser implements UserDetails {
 
     private final User user;
