23ca763208b064931ccec86d056b9f44afe9b52c
dtp06077 <dtp06077@gmail.com>
2024-07-17 22:35:17 +0900

    FEAT : JwtRequestFilter.doFilterInternal JWT 요청 필터

--- src/main/java/project/server/security/jwt/filter/JwtRequestFilter.java
+++ src/main/java/project/server/security/jwt/filter/JwtRequestFilter.java
@@ -1,7 +1,60 @@
 package project.server.security.jwt.filter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import project.server.security.constants.JwtConstants;
+import project.server.security.jwt.provider.JwtTokenProvider;
+
+import java.io.IOException;
 
 @Slf4j
-public class JwtRequestFilter {
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    /**
+     * jwt 요청 필터
+     * - request > headers > Authorization (JWT)
+     * - JWT 토큰 유효성 검사
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //헤더에서 jwt 토큰 가져오기
+        String header = request.getHeader(JwtConstants.TOKEN_HEADER);
+        log.info("authorization : " + header);
+
+        // jwt 토큰이 없음ㄴ 다음 필터로 이동
+        //Bearer + (jwt) 체크
+        if(header == null || header.length() == 0 || !header.startsWith(JwtConstants.TOKEN_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // JWT
+        // Bearer + (jwt) -> "Bearer " 제거
+        String jwt = header.replace(JwtConstants.TOKEN_PREFIX, "");
+
+        // 토큰 해석
+        Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+
+        //토큰 유효성 검사
+        if (jwtTokenProvider.validateToken(jwt)) {
+            log.info("유효한 JWT 토큰입니다.");
+
+            //로그인
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        //다음 필터
+        filterChain.doFilter(request, response);
+    }
 }
