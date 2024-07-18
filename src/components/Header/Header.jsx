40e2b81b835178c9071a75d51290659a28afaf4d
dtp06077 <dtp06077@gmail.com>
2024-07-19 03:17:13 +0900

    FEAT : Header 컴포넌트

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='logo'>
+            <Link to="/">
+                <image src="" alt="logo" className='logo'/>
+            </Link>
+        </div>
+
+        <div className='util'>
+            {/* 비로그인 시*/}
+            <ul>
+                <li><Link to = "/login">로그인</Link></li>
+                <li><Link to = "/join">회원가입</Link></li>
+                <li><Link to = "/about">소개</Link></li>
+            </ul>
+            {/* 로그인 시*/}
+            <ul>
+                <li><Link to = "/login">마이페이지</Link></li>
+                <li><button className='link'>로그아웃</button></li>
+            </ul>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
