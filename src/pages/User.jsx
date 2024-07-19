45bb27113ac9e68be9af82f4b9a17f67e82b24a1
dtp06077 <dtp06077@gmail.com>
2024-07-19 19:40:29 +0900

    FEAT : 마이 페이지

---
+++ src/pages/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <>
+            <Header />
+            <div className='container'>
+                <h1>User</h1>
+                <hr />
+                <h2>마이 페이지</h2>
+            </div>
+        </>
+  )
+}
+
+export default User
