0b2376ec874fc1ab6804c006780b51fc055df5e2
dtp06077 <dtp06077@gmail.com>
2024-07-21 16:47:07 +0900

    FEAT : logout onClick 이벤트

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,14 @@ import { LoginContext } from '../../contexts/LoginContextProvider'
 const Header = () => {
 
     //isLogin : 로그인 여부 - 로그인(true), 비로그인(false)
-    const { isLogin } = useContext(LoginContext);
+    //logout() : 로그아웃 함수 -> setLogin(false)
+    const { isLogin, logout } = useContext(LoginContext);
 
     return (
         <header>
             <div className='logo'>
                 <Link to="/">
-                    <img src="https://i.imgur.com/fzADqJo.png" alt="logo" className='logo'/>
+                    <img src="https://i.imgur.com/fzADqJo.png" alt="logo" className='logo' />
                 </Link>
             </div>
 
@@ -21,22 +22,22 @@ const Header = () => {
                     !isLogin
                         ?
                         //비로그인 시
-                    <ul>
-                        <li><Link to = "/login">로그인</Link></li>
-                        <li><Link to = "/join">회원가입</Link></li>
-                        <li><Link to = "/about">소개</Link></li>
-                    </ul>
+                        <ul>
+                            <li><Link to="/login">로그인</Link></li>
+                            <li><Link to="/join">회원가입</Link></li>
+                            <li><Link to="/about">소개</Link></li>
+                        </ul>
                         :
                         //로그인 시
-                    <ul>
-                        <li><Link to = "/user">마이페이지</Link></li>
-                        <li><button className='link'>로그아웃</button></li>
-                    </ul>
+                        <ul>
+                            <li><Link to="/user">마이페이지</Link></li>
+                            <li><button className='link' onClick={ () => logout} >로그아웃</button></li>
+                        </ul>
                 }
             </div>
 
         </header>
-  )
+    )
 }
 
 export default Header
