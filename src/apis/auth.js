cfdb1f659d5aa6ac8fe24b1de9854e42d6c3d598
dtp06077 <dtp06077@gmail.com>
2024-07-22 00:59:53 +0900

    FEAT : LoginContextProvider 로그인 상태에 따른 유저 정보 업데이트 로직 구현

---
+++ src/apis/auth.js
@@ -0,0 +1,17 @@
+import api from "./api"
+
+
+//로그인
+export const login = (loginId, password) => api.post('/login?loginId=${loginId}&password=${password}')
+
+//사용자 정보
+export const info = () => api.get('/users/${userId}') 
+
+//회원 가입
+export const join = (data) => api.post("/users", data)
+
+//회원 정보 수정
+export const update = (data) => api.put('/users', data)
+
+//회원 탈퇴
+export const remove = (userId) => api.delete("/users/${userId}")
--- src/contexts/LoginContextProvider.jsx
+++ src/contexts/LoginContextProvider.jsx
@@ -1,11 +1,71 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useState } from 'react';
+import api from '../apis/api';
+import Cookies from 'js-cookie';
 
 export const LoginContext = createContext();
 LoginContext.displayName = 'LoginContextName';
 
+/**
+ * 로그인 세팅
+ * 로그아웃 세팅
+ */
+
 const LoginContextProvider = ({children}) => {
-    // contest value : 로그인 여부, 로그아웃 함수
+    /*
+    상태
+    -로그인 여부
+    -유저 정보
+    -권한 정보
+    -쿠키 아이디 저장 여부
+    */
+
+    // 로그인 여부
     const [isLogin, setLogin] = useState(false);
+
+    // 유저 정보
+    const [userInfo, setUserInfo] = useState({});
+
+    // 권한 정보
+    const [roles, setRoles] = useState({isUser : false, isAdmin : false});
+
+    // 아이디 저장
+    const [rememberUserId, setRememberUserId] = useState();
+
+    //로그인 세팅
+    // userData, accessToken (jwt)
+    const loginSetting = (userData, accessToken) => {
+
+        const{ userId, loginId, authList }= userData
+        const roleList = authList.map((auth) => auth.auth)
+
+        console.log("userId : ${userId}");
+        console.log("loginId : ${loginId}");
+        console.log("authList : ${authList}");
+        console.log("roleList : ${roleList}");
+
+        //axios 객체의 헤더(Authorization : 'Bearer ${accessToken}')
+        api.defaults.headers.common.Authorization = 'Bearer ${accessToken}';
+    
+        //쿠키에 accessToken(jwt) 저장
+        Cookies.set("accessToken", accessToken);
+
+        //로그인 여부 : true
+        setLogin(true);
+
+        //유저정보
+        const updateUserInfo = {userId, loginId, roleList};
+        setUserInfo(updateUserInfo);
+
+        //권한정보
+        const updatedRoles = {isUser : false, isAdmin : false}
+
+        roleList.forEach((role) => {
+            if(role == 'ROLE_USER') updatedRoles.isUser = true;
+            if(role == 'ROLE_ADMIN') updatedRoles.isAdmin = true;
+        });
+
+        setRoles(updatedRoles)
+    }
 
     const logout = () => {
         setLogin(false)
