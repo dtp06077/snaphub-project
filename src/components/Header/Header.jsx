f4f847909349aa8b3b8f375c1a231297d3cecef5
dtp06077 <dtp06077@gmail.com>
2024-07-21 15:53:08 +0900

    TEST : isLogin 상태 별로 header 변경 테스트

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import { LoginContext } from '../../contexts/LoginContextProvider'
 
 const Header = () => {
-  return (
-    <header>
-        <div className='logo'>
-            <Link to="/">
-                <img src="https://i.imgur.com/fzADqJo.png" alt="logo" className='logo'/>
-            </Link>
-        </div>
 
-        <div className='util'>
-            {/* 비로그인 시*/}
-            <ul>
-                <li><Link to = "/login">로그인</Link></li>
-                <li><Link to = "/join">회원가입</Link></li>
-                <li><Link to = "/about">소개</Link></li>
-            </ul>
-            {/* 로그인 시*/}
-            <ul>
-                <li><Link to = "/user">마이페이지</Link></li>
-                <li><button className='link'>로그아웃</button></li>
-            </ul>
-        </div>
+    //isLogin : 로그인 여부 - 로그인(true), 비로그인(false)
+    const { isLogin } = useContext(LoginContext);
 
-    </header>
+    return (
+        <header>
+            <div className='logo'>
+                <Link to="/">
+                    <img src="https://i.imgur.com/fzADqJo.png" alt="logo" className='logo'/>
+                </Link>
+            </div>
+
+            <div className='util'>
+                {
+                    !isLogin
+                        ?
+                        //비로그인 시
+                    <ul>
+                        <li><Link to = "/login">로그인</Link></li>
+                        <li><Link to = "/join">회원가입</Link></li>
+                        <li><Link to = "/about">소개</Link></li>
+                    </ul>
+                        :
+                        //로그인 시
+                    <ul>
+                        <li><Link to = "/user">마이페이지</Link></li>
+                        <li><button className='link'>로그아웃</button></li>
+                    </ul>
+                }
+            </div>
+
+        </header>
   )
 }
 
