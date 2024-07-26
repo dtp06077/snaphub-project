d3cd19363f874ef6a2d3aec440d6e8608d242698
dtp06077 <dtp06077@gmail.com>
2024-07-27 00:26:23 +0900

    FEAT: UserInfoRequest DTO 생성

---
+++ src/main/java/project/server/dto/UserInfoRequest.java
@@ -0,0 +1,16 @@
+package project.server.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserInfoRequest {
+    private String name;
+    private String email;
+    private String loginId;
+    private String password;
+    private String profile;
+    private String createdAt;
+    private String auth;
+}
