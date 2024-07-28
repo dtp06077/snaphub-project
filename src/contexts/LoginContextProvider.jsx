4e497877a32d7d2b019c0cb7a4553e7e7c91ff6c
dtp06077 <dtp06077@gmail.com>
2024-07-28 17:47:57 +0900

    TEST : 로그아웃 성공 테스트

--- src/contexts/LoginContextProvider.jsx
+++ src/contexts/LoginContextProvider.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from 'react';
 import api from '../apis/api';
 import Cookies from 'js-cookie';
 import * as auth from '../apis/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginContext = createContext();
 LoginContext.displayName = 'LoginContextName';
@@ -19,7 +20,7 @@ const LoginContextProvider = ({ children }) => {
     -권한 정보
     -쿠키 아이디 저장 여부
     */
-
+    /*=====state=====*/
     // 로그인 여부
     const [isLogin, setLogin] = useState(false);
 
@@ -31,6 +32,49 @@ const LoginContextProvider = ({ children }) => {
 
     // 아이디 저장
     const [rememberUserId, setRememberUserId] = useState();
+    /*==============*/
+
+    // 페이지 이동
+    const navigate = useNavigate();
+
+    // 로그인
+    const login = async (loginId, password) => {
+
+        console.log(`loginId : ${loginId}`);
+        console.log(`password : ${password}`);
+
+        try {
+            const response = await auth.login(loginId, password);
+            const data = response.data;
+            const status = response.status;
+            const headers = response.headers;
+            const authorization = headers.authorization;
+            const accessToken = authorization.replace("Bearer ", ""); //JWT
+
+            console.log(`data : ${data}`);
+            console.log(`headers : ${headers}`);
+            console.log(`status : ${status}`);
+            console.log(`jwt : ${accessToken}`);
+
+            // 로그인 성공
+            if (status === 200) {
+                //쿠키에 accessToken(jwt) 저장
+                Cookies.set("accessToken", accessToken);
+
+                // 로그인 체크 ( /users/{userId} <--- userData)
+                loginCheck();
+
+                alert("로그인 성공");
+
+                // 메인 페이지로 이동
+                navigate("/");
+            }
+        } catch (error) {
+            // 로그인 실패
+            alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+            return;
+        }
+    }
 
     /*
     로그인 체크
@@ -41,11 +85,12 @@ const LoginContextProvider = ({ children }) => {
 
         // 쿠키에서 jwt 토큰 가져오기
         const accessToken = Cookies.get("accessToken");
-        console.log(`accessToken : ${accessToken}`); 
+        console.log(`accessToken : ${accessToken}`);
 
         // accessToken (jwt) 이 부재
         if (!accessToken) {
             console.log(`쿠키에 jwt가 없음`)
+            // 로그아웃 세팅
             logoutSetting();
             return
         }
@@ -59,45 +104,27 @@ const LoginContextProvider = ({ children }) => {
         let response
         let data
 
-        response = await auth.info();
+        try {
+            response = await auth.info();
+        } catch (error) {
+            console.log(`error : ${error}`);
+            console.log(`status : ${response.status}`);
+            return;
+        }
 
         data = response.data;
         console.log(`data : ${data}`);
 
-        //인증 성공
-        //로그인 세팅
-        loginSetting(data, accessToken)
-    }
-
-
-    // 로그인
-    const login = async (loginId, password) => {
-
-        console.log(`loginId : ${loginId}`);
-        console.log(`password : ${password}`);
-
-        const response = await auth.login(loginId, password);
-        const data = response.data;
-        const status = response.status;
-        const headers = response.headers;
-        const authorization = headers.authorization;
-        const accessToken = authorization.replace("Bearer ", ""); //JWT
-
-        console.log(`data : ${data}`);
-        console.log(`headers : ${headers}`);
-        console.log(`status : ${status}`);
-        console.log(`jwt : ${accessToken}`);
-
-        // 로그인 성공
-        if (status === 200) {
-            //쿠키에 accessToken(jwt) 저장
-            Cookies.set("accessToken", accessToken);
-
-            // 로그인 체크 ( /users/{userId} <--- userData)
-            loginCheck();
-
-            alert("로그인 성공")
+        //인증 실패
+        if (data == 'UNAUTHRIZED' || response.status == 401) {
+            console.log('사용자 인증정보 요청 실패');
+            return;
         }
+
+        //인증 성공
+        console.log(`사용자 인증정보 요청 성공`);
+        //로그인 세팅
+        loginSetting(data, accessToken);
     }
 
     //로그인 세팅
@@ -132,6 +159,20 @@ const LoginContextProvider = ({ children }) => {
         setRoles(updatedRoles)
     }
 
+    //로그아웃
+    const logout = () => {
+
+        const check = window.confirm('로그아웃 하시겠습니까?');
+
+        if (check) {
+            //로그아웃 세팅
+            logoutSetting();
+
+            //메인 페이지로 이동
+            navigate("/")
+        }
+    }
+
     //로그아웃 세팅
     const logoutSetting = () => {
         //axios 헤더 초기화
@@ -148,10 +189,6 @@ const LoginContextProvider = ({ children }) => {
 
         //권한 정보 초기화
         setRoles(null);
-    }
-
-    const logout = () => {
-        setLogin(false)
     }
 
     return (
