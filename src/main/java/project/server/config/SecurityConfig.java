971cdce6f39f2eceffb10e2f64c84cff9753bafc
dtp06077 <dtp06077@gmail.com>
2024-08-10 02:34:24 +0900

    FEAT : FailedAuthenticationEntryPoint 설정

--- src/main/java/project/server/config/SecurityConfig.java
+++ src/main/java/project/server/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package project.server.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -11,14 +14,18 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import project.server.security.custom.CustomUserDetailService;
 import project.server.security.jwt.filter.JwtAuthenticationFilter;
 import project.server.security.jwt.filter.JwtRequestFilter;
 import project.server.security.jwt.provider.JwtTokenProvider;
+
+import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -41,27 +48,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.info("시큐리티 설정");
 
-        // 폼 기반 로그인 비활성화
-        http.formLogin( (login) -> login.disable());
-        // HTTP 기본 인증 비활성화
-        http.httpBasic( (basic) -> basic.disable());
-        // CSRF 공격 방어 기능 비활성화
-        http.csrf((csrf) -> csrf.disable());
-        //세션 관리 정책 설정
-//        세션 인증을 사용하지 않고, JWT 를 사용하여 인증하기 때문에 세션 불필요
-//        http.sessionManagement( (management) -> management
-//                                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        //필터 설정
-        http.addFilterAt(new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider)
+        http
+                // 폼 기반 로그인 비활성화
+                .formLogin( (login) -> login.disable())
+                // HTTP 기본 인증 비활성화
+                .httpBasic( (basic) -> basic.disable())
+                // CSRF 공격 방어 기능 비활성화
+                .csrf((csrf) -> csrf.disable())
+                //필터 설정
+                .addFilterAt(new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider)
                         , UsernamePasswordAuthenticationFilter.class)
 
                 .addFilterBefore(new JwtRequestFilter(jwtTokenProvider)
                         , UsernamePasswordAuthenticationFilter.class)
-            ;
-
-        //인가 설정
-        http.authorizeHttpRequests( authorizeRequests ->
+                //인가 설정
+                .authorizeHttpRequests( authorizeRequests ->
                                     authorizeRequests
                                             //static 경로의 자원 접근 허용
                                             .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
@@ -73,17 +74,21 @@ public class SecurityConfig {
                                             .requestMatchers("/search/**").permitAll()
                                             .requestMatchers("/file/**").permitAll()
                                             .requestMatchers("/admin/**").hasRole("ADMIN")
-                                            .anyRequest().authenticated());
-
-        /**
-         * 인증 방식 설정
-         * 인메모리 방식
-         * JDBC 방식
-         * 커스텀 방식 * -> userDetailService
-         */
-        http.userDetailsService(customUserDetailService);
-
-
+                                            .anyRequest().authenticated())
+                //인증 실패 설정
+                .exceptionHandling(exceptionConfig ->
+                                    exceptionConfig.authenticationEntryPoint(new FailedAuthenticationEntryPoint()))
+                /**
+                 * 인증 방식 설정
+                 * 인메모리 방식
+                 * JDBC 방식
+                 * 커스텀 방식 * -> userDetailService
+                 */
+                .userDetailsService(customUserDetailService);
+                //세션 관리 정책 설정
+//              세션 인증을 사용하지 않고, JWT 를 사용하여 인증하기 때문에 세션 불필요
+//              .sessionManagement( (management) -> management
+//                                           .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
     //PasswordEncoder 빈 등록
@@ -102,5 +107,17 @@ public class SecurityConfig {
 
         this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
         return authenticationManager;
+    }
+}
+
+//인증 실패시
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{ \"code\": \"NP\", \"message\": \"Do not have permission.\" }");
     }
 }
