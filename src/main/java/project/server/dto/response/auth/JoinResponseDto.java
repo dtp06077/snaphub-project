d538e7a04a06cc6eef07a08368410f86fed204da
dtp06077 <dtp06077@gmail.com>
2024-08-16 02:03:27 +0900

    FEAT : JoinResponseDto

--- src/main/java/project/server/dto/response/auth/JoinResponseDto.java
+++ src/main/java/project/server/dto/response/auth/JoinResponseDto.java
@@ -1,4 +1,52 @@
 package project.server.dto.response.auth;
 
-public class JoinResponseDto {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import project.server.common.ResponseCode;
+import project.server.common.ResponseMessage;
+import project.server.dto.response.ResponseDto;
+
+@Getter
+public class JoinResponseDto extends ResponseDto {
+
+    private JoinResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+    //HTTP Status 200
+    public static ResponseEntity<JoinResponseDto> success() {
+        JoinResponseDto result = new JoinResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    //HTTP Status 400
+    public static ResponseEntity<ResponseDto> duplicateId() {
+        ResponseDto result = new ResponseDto(ResponseCode.DUPLICATE_ID, ResponseMessage.DUPLICATE_ID);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> missingId() {
+        ResponseDto result = new ResponseDto(ResponseCode.MISSING_ID, ResponseMessage.MISSING_ID);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> missingPassword() {
+        ResponseDto result = new ResponseDto(ResponseCode.MISSING_PASSWORD, ResponseMessage.MISSING_PASSWORD);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> missingPasswordCheck() {
+        ResponseDto result = new ResponseDto(ResponseCode.MISSING_PASSWORD_CHECK, ResponseMessage.MISSING_PASSWORD_CHECK);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> missingName() {
+        ResponseDto result = new ResponseDto(ResponseCode.MISSING_NAME, ResponseMessage.MISSING_NAME);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> duplicateName() {
+        ResponseDto result = new ResponseDto(ResponseCode.DUPLICATE_NAME, ResponseMessage.DUPLICATE_NAME);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
 }
