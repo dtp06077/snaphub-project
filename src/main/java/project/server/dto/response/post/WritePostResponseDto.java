e40a06b18cec9dcd1cb9c832ce4d1aa277453c73
dtp06077 <dtp06077@gmail.com>
2024-08-28 18:05:12 +0900

    FEAT : WritePostResponseDto

--- src/main/java/project/server/dto/response/post/WritePostResponseDto.java
+++ src/main/java/project/server/dto/response/post/WritePostResponseDto.java
@@ -1,9 +1,28 @@
 package project.server.dto.response.post;
 
-import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import project.server.common.ResponseCode;
+import project.server.common.ResponseMessage;
 import project.server.dto.response.ResponseDto;
 
-@Data
-
+@Getter
 public class WritePostResponseDto extends ResponseDto {
+
+    private WritePostResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    //Http Status 200
+    public static ResponseEntity<WritePostResponseDto> success() {
+        WritePostResponseDto result = new WritePostResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    //HTTP Status 401
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
 }
