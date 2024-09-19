d34e18728aa1047c3ff62dadb4e7e57bb7c10894
dtp06077 <dtp06077@gmail.com>
2024-09-19 17:53:22 +0900

    FEAT : GetCommentsResponseDto 구현

--- src/main/java/project/server/dto/object/CommentListItem.java
+++ src/main/java/project/server/dto/object/CommentListItem.java
@@ -3,6 +3,11 @@ package project.server.dto.object;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import project.server.domain.Comment;
+import project.server.domain.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +18,22 @@ public class CommentListItem {
     private String profileImage;
     private String commentDateTime;
     private String content;
+
+    public CommentListItem(Comment comment) {
+        this.loginId = comment.getAuthor().getLoginId();
+        this.name = comment.getAuthor().getName();
+        this.profileImage = comment.getAuthor().getProfileImage();
+        this.commentDateTime = comment.getCommentDatetime();
+        this.content = comment.getContent();
+    }
+
+    public static List<CommentListItem> copyList(Post post) {
+        List<CommentListItem> commentList = new ArrayList<>();
+
+        for(Comment comment : post.getComments()) {
+            CommentListItem item = new CommentListItem(comment);
+            commentList.add(item);
+        }
+        return commentList;
+    }
 }
---
+++ src/main/java/project/server/dto/response/post/GetCommentsResponseDto.java
@@ -0,0 +1,33 @@
+package project.server.dto.response.post;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import project.server.common.ResponseCode;
+import project.server.common.ResponseMessage;
+import project.server.domain.Post;
+import project.server.dto.object.CommentListItem;
+import project.server.dto.response.ResponseDto;
+
+import java.util.List;
+
+@Getter
+public class GetCommentsResponseDto extends ResponseDto {
+
+    private List<CommentListItem> commentList;
+
+    private GetCommentsResponseDto(Post post) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.commentList = CommentListItem.copyList(post);
+    }
+
+    public static ResponseEntity<GetCommentsResponseDto> success(Post post) {
+        GetCommentsResponseDto result = new GetCommentsResponseDto(post);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistPost() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_POST, ResponseMessage.NOT_EXISTED_POST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
