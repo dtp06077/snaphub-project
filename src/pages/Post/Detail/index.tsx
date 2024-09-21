56744f263df3d5d38229884f76eac4d285b662bb
dtp06077 <dtp06077@gmail.com>
2024-09-22 03:00:14 +0900

    FEAT : 게시물 상세 이벤트 처리

--- src/pages/Post/Detail/index.tsx
+++ src/pages/Post/Detail/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import EmotionItem from '../../../components/EmotionItem';
 import { CommentListItem, EmotionListItem, Post } from '../../../types/interface';
 import { commentListMock, emotionListMock } from '../../../mocks';
@@ -7,7 +7,7 @@ import Pagination from '../../../components/Pagination';
 import './style.css';
 import { useLoginUserStore } from '../../../stores';
 import { useNavigate, useParams } from 'react-router-dom';
-import { POST_PATH, POST_UPDATE_PATH, USER_PATH } from '../../../constants';
+import { MAIN_PATH, POST_PATH, POST_UPDATE_PATH, USER_PATH } from '../../../constants';
 import defaultImage from '../assets/image/default-profile-image.png';
 
 //component: 게시물 상세 화면 컴포넌트
@@ -32,10 +32,10 @@ export default function PostDetail() {
 
         //event handler: 닉네임 클릭 이벤트 처리
         const onNicknameClickHandler = () => {
-            if(!post) return;
+            if (!post) return;
             navigator(USER_PATH(post.posterId));
         }
-        
+
         //event handler: more 버튼 클릭 이벤트 처리
         const onMoreButtonClickHandler = () => {
             setShowMore(!showMore);
@@ -43,20 +43,28 @@ export default function PostDetail() {
 
         //event handler: 수정 버튼 클릭 이벤트 처리
         const onUpdateButtonClickHandler = () => {
-            if(!post || !loginUser) return;
-            if(loginUser.loginId !== post.posterId) return;
+            if (!post || !loginUser) return;
+            if (loginUser.loginId !== post.posterId) return;
             navigator(POST_PATH() + '/' + POST_UPDATE_PATH(post.postId));
         }
 
+        //event handler: 삭제 버튼 클릭 이벤트 처리
+        const onDeleteButtonClickHandler = () => {
+            if (!post || !loginUser) return;
+            if (loginUser.loginId !== post.posterId) return;
+            //TODO: Delete Request
+            navigator(MAIN_PATH());
+        }
+
         //render: 게시물 상세 상단 컴포넌트 렌더링
-        if(!post) return <></>;
-        return(
-            <div id = 'post-detail-top'>
+        if (!post) return <></>;
+        return (
+            <div id='post-detail-top'>
                 <div className='post-detail-top-header'>
                     <div className='post-detail-title'>{post.title}</div>
                     <div className='post-detail-top-sub-box'>
                         <div className='post-detail-write-info-box'>
-                            <div className='post-detail-writer-profile-image' style={{backgroundImage: `url(${post.posterProfileImage ? post.posterProfileImage : defaultImage})`}}></div>
+                            <div className='post-detail-writer-profile-image' style={{ backgroundImage: `url(${post.posterProfileImage ? post.posterProfileImage : defaultImage})` }}></div>
                             <div className='post-detail-writer-nickname' onClick={onNicknameClickHandler}>{post.posterName}</div>
                             <div className='post-detail-info-divider'>{`\|`}</div>
                             <div className='post-detail-write-date'>{post.postDateTime}</div>
@@ -65,17 +73,17 @@ export default function PostDetail() {
                             <div className='icon more-icon'></div>
                         </div>
                         {showMore &&
-                        <div className='post-detail-more-box'>
-                            <div className='post-detail-update-button' onClick={onUpdateButtonClickHandler}>{`수정`}</div>
-                            <div className='divider'></div>
-                            <div className='post-detail-delete-button'>{`삭제`}</div>
-                        </div> }
+                            <div className='post-detail-more-box'>
+                                <div className='post-detail-update-button' onClick={onUpdateButtonClickHandler}>{`수정`}</div>
+                                <div className='divider'></div>
+                                <div className='post-detail-delete-button' onClick={onDeleteButtonClickHandler}>{`삭제`}</div>
+                            </div>}
                     </div>
                 </div>
                 <div className='divider'></div>
                 <div className='post-detail-top-main'>
                     <div className='post-detail-main-text'>{post.content}</div>
-                    {post.imageList.map(image => <img className='post-detail-main-image' src={image}/>)}
+                    {post.imageList.map(image => <img className='post-detail-main-image' src={image} />)}
                 </div>
             </div>
         );
@@ -84,74 +92,131 @@ export default function PostDetail() {
     //component: 게시물 상세 하단 컴포넌트
     const PostDetailBottom = () => {
 
-        const [emotionList, setEmotionList] = useState<EmotionListItem[]>([]);
-        const [commentList, setCommentList] = useState<CommentListItem[]>([]);
+        //state: 댓글 textarea 참조 상태
+        const commentRef = useRef<HTMLTextAreaElement | null>(null);
 
+        //state: 감정표현 리스트 상태
+        const [emotionList, setEmotionList] = useState<EmotionListItem[]>([]);
+        //state: 댓글 리스트 상태
+        const [commentList, setCommentList] = useState<CommentListItem[]>([]);
+        //state: 감정표현 상태
+        const [isEmotion, setEmotion] = useState<boolean>(false);
+        //state: 댓글 상태
+        const [comment, setComment] = useState<string>('');
+        //state: 감정표현 리스트 보기 상태
+        const [showEmotion, setShowEmotion] = useState<boolean>(false);
+        //state: 댓글 리스트 보기 상태
+        const [showComment, setShowComment] = useState<boolean>(false);
+
+
+        //event handler: 감정표현 버튼 클릭 이벤트 처리
+        const onEmotionClickHandler = () => {
+            setEmotion(!isEmotion);
+        }
+
+        //event handler: 감정표현 리스트 보기 클릭 이벤트 처리
+        const onShowEmotionClickHandler = () => {
+            setShowEmotion(!showEmotion);
+        }
+
+        //event handler: 댓글 리스트 보기 클릭 이벤트 처리
+        const onShowCommentClickHandler = () => {
+            setShowComment(!showComment);
+        }
+
+        //event handler: 댓글 작성 버튼 클릭 이벤트 처리
+        const onCommentSubmitButtonClickHandler = () => {
+            if (!comment) return;
+        }
+
+        //event handler: 댓글 변경 이벤트 처리
+        const onCommentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+            const { value } = event.target;
+            setComment(value);
+            if(!commentRef.current) return;
+            commentRef.current.style.height = 'auto';
+            commentRef.current.style.height = `${commentRef.current.scrollHeight}px`;
+        }
+
+        //effect: 게시물 번호 path variable이 바뀔 때 마다 좋아요 및 댓글 리스트 불러오기
         useEffect(() => {
             setEmotionList(emotionListMock);
             setCommentList(commentListMock);
-        }, []);
+        }, [postId]);
 
         //render: 게시물 상세 하단 컴포넌트 렌더링
-        return(
+        return (
             <div id='post-detail-bottom'>
                 <div className='post-detail-bottom-button-box'>
                     <div className='post-detail-bottom-button-group'>
-                        <div className='icon-button'>
-                            <div className='icon emotion-fill-icon'></div>
+                        <div className='icon-button' onClick={onEmotionClickHandler}>
+                            {isEmotion ?
+                                <div className='icon emotion-fill-icon'></div> :
+                                <div className='icon emotion-light-icon'></div>
+                            }
                         </div>
-                        <div className='post-detail-bottom-button-text'>{`좋아요 ${1}`}</div>
-                        <div className='icon-button'>
-                            <div className='icon up-light-icon'></div>
+                        <div className='post-detail-bottom-button-text'>{`좋아요 `}<span className='emphasis'>{emotionList.length}</span></div>
+                        <div className='icon-button' onClick={onShowEmotionClickHandler}>
+                            {showEmotion ?
+                                <div className='icon up-light-icon'></div> :
+                                <div className='icon down-light-icon'></div>
+                            }
                         </div>
                     </div>
                     <div className='post-detail-bottom-button-group'>
                         <div className='icon-button'>
-                            <div className='icon comment-fill-icon'></div>
+                            <div className='icon comment-icon'></div>
                         </div>
-                        <div className='post-detail-bottom-button-text'>{`댓글 ${1}`}</div>
-                        <div className='icon-button'>
-                            <div className='icon up-light-icon'></div>
-                        </div>
-                    </div>
-                </div>
-                <div className='post-detail-bottom-emotion-box'>
-                    <div className='post-detail-bottom-emotion-container'>
-                        <div className='post-detail-bottom-emotion-title'>{`좋아요`}<span className='emphasis'>{1}</span></div>
-                        <div className='post-detail-bottom-emotion-contents'>
-                            {emotionList.map(item => <EmotionItem emotionListItem={item}/>)}
+                        <div className='post-detail-bottom-button-text'>{`댓글 `}<span className='emphasis'>{commentList.length}</span></div>
+                        <div className='icon-button' onClick={onShowCommentClickHandler}>
+                            {showComment ?
+                                <div className='icon up-light-icon'></div> :
+                                <div className='icon down-light-icon'></div>
+                            }
                         </div>
                     </div>
                 </div>
-                <div className='post-detail-bottom-comment-box'>
-                    <div className='post-detail-bottom-comment-container'>
-                        <div className='post-detail-bottom-comment-title'>{`댓글`}<span className='emphasis'>{1}</span></div>
-                        <div className='post-detail-bottom-comment-list-container'>
-                            {commentList.map(item => <CommentItem commentListItem={item}/>)}
-                        </div>
-                    </div>
-                    <div className='divider'></div>
-                    <div className='post-detail-bottom-comment-pagination-box'>
-                        <Pagination/>
-                    </div>
-                    <div className='post-detail-bottom-comment-input-box'>
-                        <div className='post-detail-bottom-comment-input-container'>
-                            <textarea className="post-detail-bottom-comment-textarea" placeholder='댓글을 작성해주세요.'></textarea>
-                            <div className='post-detail-bottom-comment-button-box'>
-                                <div className='disable-button'>{`댓글달기`}</div>
+                {showEmotion &&
+                    <div className='post-detail-bottom-emotion-box'>
+                        <div className='post-detail-bottom-emotion-container'>
+                            <div className='post-detail-bottom-emotion-title'>{`좋아요`}<span className='emphasis'>{emotionList.length}</span></div>
+                            <div className='post-detail-bottom-emotion-contents'>
+                                {emotionList.map(item => <EmotionItem emotionListItem={item} />)}
                             </div>
                         </div>
                     </div>
-                </div>
+                }
+                {showComment &&
+                    <div className='post-detail-bottom-comment-box'>
+                        <div className='post-detail-bottom-comment-container'>
+                            <div className='post-detail-bottom-comment-title'>{`댓글`}<span className='emphasis'>{commentList.length}</span></div>
+                            <div className='post-detail-bottom-comment-list-container'>
+                                {commentList.map(item => <CommentItem commentListItem={item} />)}
+                            </div>
+                        </div>
+                        <div className='divider'></div>
+                        <div className='post-detail-bottom-comment-pagination-box'>
+                            <Pagination />
+                        </div>
+                        <div className='post-detail-bottom-comment-input-box'>
+                            <div className='post-detail-bottom-comment-input-container'>
+                                <textarea ref={commentRef} className="post-detail-bottom-comment-textarea" placeholder='댓글을 작성해주세요.' value={comment} onChange={onCommentChangeHandler}></textarea>
+                                <div className='post-detail-bottom-comment-button-box'>
+                                    <div className={comment === '' ? 'disable-button' : 'black-button'} onClick={onCommentSubmitButtonClickHandler}>{`댓글달기`}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         );
     };
     //render: 게시물 상세 화면 컴포넌트 렌더링
     return (
-        <div id = 'post-detail-wrapper'>
+        <div id='post-detail-wrapper'>
             <div className='post-detail-container'>
-                <PostDetailTop/>
-                <PostDetailBottom/>
+                <PostDetailTop />
+                <PostDetailBottom />
             </div>
         </div>
     )
