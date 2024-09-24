599200c61509bef2dd7d00b05e0aa714417707c2
dtp06077 <dtp06077@gmail.com>
2024-09-25 05:42:31 +0900

    FEAT : 댓글 작성일 경과 함수 구현

--- src/components/CommentItem/index.tsx
+++ src/components/CommentItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.css'
 import { CommentListItem } from '../../types/interface'
 import defaultProfileImage from "../../assets/image/default-profile-image.png";
+import dayjs from 'dayjs';
 
 interface Props {
     commentListItem: CommentListItem;
@@ -10,9 +11,20 @@ interface Props {
 //component: Comment List Item 컴포넌트
 export default function CommentItem({ commentListItem }: Props) {
 
-    //properties
+    //state: properties
     const { name, profileImage, commentDateTime, content } = commentListItem;
 
+    //function: 작성일 경과시간 함수
+    const getTime = () => {
+        const now = dayjs().add(9, 'hour');
+        const writeTime = dayjs(commentDateTime);
+        
+        const gap = now.diff(writeTime, 's');
+        if(gap < 60) return `${gap}초 전`;
+        if(gap < 3600) return `${Math.floor(gap / 60)}분 전`;
+        if(gap < 86400) return `${Math.floor(gap/3600)}시간 전`;
+        return `${Math.floor(gap/86400)}일 전`;
+    }
     //render: Comment List Item 렌더링
     return (
         <div className='comment-list-item'>
@@ -22,7 +34,7 @@ export default function CommentItem({ commentListItem }: Props) {
                 </div>
                 <div className='comment-list-item-name'>{name}</div>
                 <div className='comment-list-item-divider'>{`\|`}</div>
-                <div className='comment-list-item-time'>{commentDateTime}</div>
+                <div className='comment-list-item-time'>{getTime()}</div>
             </div>
             <div className='comment-list-item-main'>
                 <div className='comment-list-item-content'>{content}</div>
