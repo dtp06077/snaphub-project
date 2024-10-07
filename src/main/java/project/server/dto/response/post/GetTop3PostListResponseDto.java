6df8c858f8de5f0f010d167ab39c0bf52e6cda1a
dtp06077 <dtp06077@gmail.com>
2024-10-07 17:58:12 +0900

    FEAT : GetTop3PostListResponseDto 생성

--- src/main/java/project/server/dto/response/post/GetTop3PostListResponseDto.java
+++ src/main/java/project/server/dto/response/post/GetTop3PostListResponseDto.java
@@ -12,17 +12,17 @@ import project.server.dto.response.ResponseDto;
 import java.util.List;
 
 @Getter
-public class GetLatestPostListResponseDto extends ResponseDto {
+public class GetTop3PostListResponseDto extends ResponseDto {
 
-    private List<PostListItem> latestList;
+    private List<PostListItem> top3List;
 
-    private GetLatestPostListResponseDto(List<Post> latestList) {
+    private GetTop3PostListResponseDto(List<Post> top3List) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-        this.latestList = PostListItem.copyList(latestList);
+        this.top3List = PostListItem.copyList(top3List);
     }
 
-    public static ResponseEntity<GetLatestPostListResponseDto> success(List<Post> latestList) {
-        GetLatestPostListResponseDto result = new GetLatestPostListResponseDto(latestList);
+    public static ResponseEntity<GetTop3PostListResponseDto> success(List<Post> top3List) {
+        GetTop3PostListResponseDto result = new GetTop3PostListResponseDto(top3List);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
