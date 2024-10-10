31d38d6d7b797085a2867436a91c6969ae4b42d4
dtp06077 <dtp06077@gmail.com>
2024-10-10 16:09:55 +0900

    FEAT : 검색어 로그 저장 기능 구현

--- src/main/java/project/server/repository/SearchLogRepository.java
+++ src/main/java/project/server/repository/SearchLogRepository.java
@@ -3,6 +3,7 @@ package project.server.repository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import project.server.domain.SearchLog;
 
 import java.util.List;
 
@@ -12,6 +13,12 @@ public class SearchLogRepository {
 
     private final EntityManager em;
 
+    //검색어 로그 저장
+    public int save(SearchLog searchLog) {
+        em.persist(searchLog);
+        return searchLog.getSequence();
+    }
+    //인기 검색어 리스트 검색
     public List<String> findPopularSearch() {
         return em.createQuery("select sl.searchWord from SearchLog sl " +
                 "group by sl.searchWord " +
