fadfeb70f43e45decfca2bd13dc50d0e63cb8064
dtp06077 <dtp06077@gmail.com>
2024-10-12 23:45:53 +0900

    FEAT : 연관 검색어 리스트 검색 서비스 구현

--- src/main/java/project/server/service/Implement/SearchServiceImpl.java
+++ src/main/java/project/server/service/Implement/SearchServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.server.dto.response.ResponseDto;
 import project.server.dto.response.search.GetPopularSearchListResponseDto;
+import project.server.dto.response.search.GetRelatableSearchListResponseDto;
 import project.server.repository.SearchLogRepository;
 import project.server.service.SearchService;
 
@@ -24,13 +25,29 @@ public class SearchServiceImpl implements SearchService {
         List<String> popularSearchList;
 
         try {
-
             popularSearchList = searchLogRepository.findPopularSearch();
 
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseDto.databaseError();
         }
 
         return GetPopularSearchListResponseDto.success(popularSearchList);
+    }
+
+    @Override
+    public ResponseEntity<? super GetRelatableSearchListResponseDto> getRelatableSearchList(String searchWord) {
+
+        List<String> relatableSearchList;
+
+        try {
+            relatableSearchList = searchLogRepository.findRelationSearch(searchWord);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetRelatableSearchListResponseDto.success(relatableSearchList);
     }
 }
--- src/main/java/project/server/service/SearchService.java
+++ src/main/java/project/server/service/SearchService.java
@@ -2,8 +2,13 @@ package project.server.service;
 
 import org.springframework.http.ResponseEntity;
 import project.server.dto.response.search.GetPopularSearchListResponseDto;
+import project.server.dto.response.search.GetRelatableSearchListResponseDto;
 
 public interface SearchService {
 
+    //인기 검색어 리스트 불러오기
     ResponseEntity<? super GetPopularSearchListResponseDto> getPopularSearchList();
+
+    //연관 검색어 리스트 불러오기
+    ResponseEntity<? super GetRelatableSearchListResponseDto> getRelatableSearchList(String searchWord);
 }
