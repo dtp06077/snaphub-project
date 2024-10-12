d95a6d9e0d57494ca70b89a3390e232d4f4b49b6
dtp06077 <dtp06077@gmail.com>
2024-10-13 00:02:40 +0900

    FEAT : 연관 검색어 리스트 검색 컨트롤러 계층 구현

--- src/main/java/project/server/controller/SearchController.java
+++ src/main/java/project/server/controller/SearchController.java
@@ -3,9 +3,11 @@ package project.server.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.server.dto.response.search.GetPopularSearchListResponseDto;
+import project.server.dto.response.search.GetRelatableSearchListResponseDto;
 import project.server.service.SearchService;
 
 @RestController
@@ -21,5 +23,14 @@ public class SearchController {
     @GetMapping("/popular-list")
     public ResponseEntity<? super GetPopularSearchListResponseDto> getPopularSearchList() {
         return searchService.getPopularSearchList();
+    }
+
+    /**
+     * 연관 검색어 리스트 검색
+     */
+    @GetMapping("/{searchWord}/relation-list")
+    public ResponseEntity<? super GetRelatableSearchListResponseDto> getRelatableSearchList(
+            @PathVariable("searchWord") String searchWord) {
+        return searchService.getRelatableSearchList(searchWord);
     }
 }
