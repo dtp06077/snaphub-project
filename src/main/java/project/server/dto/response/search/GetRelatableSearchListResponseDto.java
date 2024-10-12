d03f1b592855c596482170131d196ded66def978
dtp06077 <dtp06077@gmail.com>
2024-10-12 23:22:59 +0900

    FEAT : GetRelatableSearchListResponseDto 생성

---
+++ src/main/java/project/server/dto/response/search/GetRelatableSearchListResponseDto.java
@@ -0,0 +1,26 @@
+package project.server.dto.response.search;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import project.server.common.ResponseCode;
+import project.server.common.ResponseMessage;
+import project.server.dto.response.ResponseDto;
+
+import java.util.List;
+
+@Getter
+public class GetRelatableSearchListResponseDto extends ResponseDto {
+
+    private List<String> relatedSearchList;
+
+    private GetRelatableSearchListResponseDto(List<String> relatedSearchList) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.relatedSearchList = relatedSearchList;
+    }
+
+    public static ResponseEntity<GetRelatableSearchListResponseDto> success(List<String> relatedSearchList) {
+        GetRelatableSearchListResponseDto result = new GetRelatableSearchListResponseDto(relatedSearchList);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
