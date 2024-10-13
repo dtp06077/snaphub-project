136b2633edd706d5e0dcdd592244ff91466fa567
dtp06077 <dtp06077@gmail.com>
2024-10-14 01:01:02 +0900

    FEAT : GetRelatableSearchListResponseDto 생성

--- src/apis/response/search/index.ts
+++ src/apis/response/search/index.ts
@@ -1,5 +1,7 @@
 import GetPopularSearchListResponseDto from "./get-popular-search-list.response.dto";
+import GetRelatableSearchListResponseDto from "./get-relatable-search-list.response.dto";
 
 export type {
-    GetPopularSearchListResponseDto
+    GetPopularSearchListResponseDto,
+    GetRelatableSearchListResponseDto
 }
