be3b18c8435e5327ece3377e5d1de76a48792b38
dtp06077 <dtp06077@gmail.com>
2024-10-14 03:01:40 +0900

    FEAT : GetUserPostListResponseDto 생성

---
+++ src/main/java/project/server/dto/response/post/GetUserPostListResponseDto.java
@@ -0,0 +1,33 @@
+package project.server.dto.response.post;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import project.server.common.ResponseCode;
+import project.server.common.ResponseMessage;
+import project.server.domain.Post;
+import project.server.dto.object.PostListItem;
+import project.server.dto.response.ResponseDto;
+
+import java.util.List;
+
+@Getter
+public class GetUserPostListResponseDto extends ResponseDto {
+
+    private List<PostListItem> userPostList;
+
+    private GetUserPostListResponseDto(List<Post> userPostList) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.userPostList = PostListItem.copyList(userPostList);
+    }
+
+    public static ResponseEntity<GetUserPostListResponseDto> success(List<Post> userPostList) {
+        GetUserPostListResponseDto result = new GetUserPostListResponseDto(userPostList);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+}
