cdb9910492f63784c52df929b11af65344df9765
dtp06077 <dtp06077@gmail.com>
2024-10-15 03:23:41 +0900

    FEAT : 닉네임 및 프로필 이미지 수정 컨트롤러 계층 구현

--- src/main/java/project/server/controller/UserController.java
+++ src/main/java/project/server/controller/UserController.java
@@ -1,12 +1,17 @@
 package project.server.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import project.server.dto.request.user.UpdateNameRequestDto;
+import project.server.dto.request.user.UpdateProfileImageRequestDto;
 import project.server.dto.response.user.GetUserInfoResponseDto;
+import project.server.dto.response.user.UpdateNameResponseDto;
+import project.server.dto.response.user.UpdateProfileImageResponseDto;
 import project.server.security.domain.CustomUser;
 import project.server.service.UserService;
 
@@ -23,48 +28,27 @@ public class UserController {
      */
     @Secured("ROLE_USER") //USER 권한 설정
     @GetMapping("/info")
-    public ResponseEntity<? super GetUserInfoResponseDto> userInfo(@AuthenticationPrincipal CustomUser customUser) {
-
+    public ResponseEntity<? super GetUserInfoResponseDto> getUserInfo(@AuthenticationPrincipal CustomUser customUser) {
         return userService.getUserInfo(customUser);
     }
 
     /**
-     * 회원 정보 수정
+     * 닉네임 수정
      */
-//    @Secured("ROLE_USER") //USER 권한 설정
-//    @PutMapping("")
-//    public ResponseEntity<?> update(@RequestBody UserUpdateRequest request) throws Exception {
-//        log.info("[PUT] - /users");
-//        int result = userService.update(request);
-//
-//        if( result >= 0 ) {
-//            log.info("회원수정 성공! - SUCCESS");
-//            return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
-//        }
-//        else {
-//            log.info("회원수정 실패! - FAIL");
-//            return new ResponseEntity<>("FAIL", HttpStatus.BAD_REQUEST);
-//        }
-//    }
+    @Secured("ROLE_USER")
+    @PatchMapping("/name")
+    public ResponseEntity<? super UpdateNameResponseDto> updateName(
+            @RequestBody @Valid UpdateNameRequestDto request, @AuthenticationPrincipal CustomUser customUser) {
+        return userService.updateName(request, customUser);
+    }
 
     /**
-     * 회원 삭제
+     * 프로필 이미지 수정
      */
-//    @Secured("ROLE_ADMIN")
-//    @DeleteMapping("/{loginId}")
-//    public ResponseEntity<?> destroy(@PathVariable("loginId") String loginId) throws Exception {
-//        log.info("[DELETE] - /users/{userId}");
-//
-//        int result = userService.delete(loginId);
-//
-//        if( result >= 0 ) {
-//            log.info("회원삭제 성공! - SUCCESS");
-//            return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
-//        }
-//        else {
-//            log.info("회원삭제 실패! - FAIL");
-//            return new ResponseEntity<>("FAIL", HttpStatus.BAD_REQUEST);
-//        }
-//
-//    }
+    @Secured("ROLE_USER")
+    @PatchMapping("/name")
+    public ResponseEntity<? super UpdateProfileImageResponseDto> updateProfileImage(
+            @RequestBody @Valid UpdateProfileImageRequestDto request, @AuthenticationPrincipal CustomUser customUser) {
+        return userService.updateProfileImage(request, customUser);
+    }
 }
