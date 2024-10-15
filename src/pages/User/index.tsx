cfd960b74b9fc735b3a71872e8a5f7d33b31736c
dtp06077 <dtp06077@gmail.com>
2024-10-16 04:51:41 +0900

    FEAT : 사용자 페이지 상단 컴포넌트 스타일 및 이벤트 처리

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import defaultProfileImage from "../../assets/image/default-profile-image.png";
 import { useParams } from 'react-router-dom';
 import './style.css';
@@ -15,7 +15,7 @@ export default function User() {
         //state: 이미지 파일 인풋 참조 상태
         const imageInputRef = useRef<HTMLInputElement | null>(null);
         //state: 마이페이지 여부 상태
-        const [isMyPage, setMyPage] = useState<boolean>(false);
+        const [isMyPage, setMyPage] = useState<boolean>(true);
         //state: 닉네임 변경 여부 상태
         const [isNameChanged, setIsNameChanged] = useState<boolean>(false);
         //state: 닉네임 상태
@@ -24,6 +24,34 @@ export default function User() {
         const [changeName, setChangeName] = useState<string>('');
         //state: 프로필 이미지 상태
         const [profileImage, setProfileImage] = useState<string | null>(null);
+
+        //event handler: 프로필 박스 클릭 이벤트 처리
+        const onProfileBoxClickHandler = () => {
+            if (!isMyPage) return;
+            if (!imageInputRef.current) return;
+            imageInputRef.current.click();
+        };
+
+        //event handler: 닉네임 수정 버튼 클릭 이벤트 처리
+        const onNameEditButtonClickHandler = () => {
+            setChangeName(name);
+            setIsNameChanged(!isNameChanged);
+        };
+
+        //event handler: 프로필 이미지 변경 이벤트 처리
+        const onProfileImageChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+            if (!event.target.files || !event.target.files?.length) return;
+
+            const file = event.target.files[0];
+            const data = new FormData();
+            data.append('file', file);
+        };
+
+        //event handler: 닉네임 변경 이벤트 처리
+        const onNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+            const { value } = event.target;
+            setChangeName(value);
+        };
 
         // useEffect: name path variable 변경 시 실행 할 함수
         useEffect(() => {
@@ -37,16 +65,16 @@ export default function User() {
             <div id="user-top-wrapper">
                 <div className='user-top-container'>
                     {isMyPage ?
-                        <div className='user-top-my-profile-image-box'>
+                        <div className='user-top-my-profile-image-box' onClick={onProfileBoxClickHandler}>
                             {profileImage !== null ?
                                 <div className='user-top-my-profile-image' style={{ backgroundImage: `url(${profileImage})` }}></div> :
-                                <div className='user-top-my-profile-image-nothing-box'>
-                                    <div className='icon-box-large'>
-                                        <div className='icon image-box-white-icon'></div>
-                                    </div>
+
+                                <div className='icon-box-large'>
+                                    <div className='icon image-box-white-icon'></div>
                                 </div>
+
                             }
-                            <input ref={imageInputRef} type='file' accept='image/*' style={{ display: 'none' }} />
+                            <input ref={imageInputRef} type='file' accept='image/*' style={{ display: 'none' }} onChange={onProfileImageChangeHandler} />
                         </div> :
                         <div className='user-top-profile-image-box' style={{ backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})` }}></div>
                     }
@@ -55,10 +83,10 @@ export default function User() {
                             {isMyPage ?
                                 <>
                                     {isNameChanged ?
-                                        <input className='user-top-info-name-input' type='text' size={changeName.length + 1} value={changeName} /> :
+                                        <input className='user-top-info-name-input' type='text' size={changeName.length + 1} value={changeName} onChange={onNameChangeHandler}/> :
                                         <div className='user-top-info-name'>{name}</div>
                                     }
-                                    <div className='icon-button'>
+                                    <div className='icon-button' onClick={onNameEditButtonClickHandler}>
                                         <div className='icon edit-icon'></div>
                                     </div>
                                 </> :
