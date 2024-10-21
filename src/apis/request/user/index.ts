8c8f0e84b2712d07781aa825a49ecda746e58bdf
dtp06077 <dtp06077@gmail.com>
2024-10-21 16:39:44 +0900

    FEAT : 닉네임 수정, 프로필 이미지 수정 리퀘스트

---
+++ src/apis/request/user/index.ts
@@ -0,0 +1,7 @@
+import UpdateNameRequestDto from "./update-name.request.dto";
+import UpdateProfileImageRequestDto from "./update-profile-image.request.dto";
+
+export type {
+    UpdateNameRequestDto,
+    UpdateProfileImageRequestDto
+}
